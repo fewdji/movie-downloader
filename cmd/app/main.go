5832e6fb7b919145a76df10d363f654c98a3bc21
fewdji <fewdji@gmail.com>
2023-10-24 19:01:13 +0300

    env, simple tg bot

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("It works!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tgBotToken := os.Getenv("TG_BOT_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(tgBotToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on %s", bot.Self.UserName)
+
+	updateConfig := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
